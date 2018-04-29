2fd3ae5774e9682c625a8db3831086f502215092
arehmandev <arehmandev@outlook.com>
2018-04-29 03:11:13 +0100

    Test ensures kd lists by numbers -> alphabet

--- examples/example.sh
+++ examples/example.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 export NGINX_IMAGE_TAG="1.11-alpine"
-kd -c mykube -namespace testing -f nginx-deployment.yaml --debug-templates --dryrun
+#kd -c mykube -namespace testing -f nginx-deployment.yaml --debug-templates --dryrun
 
 export LIST="ENTRY1,ENTRY2,ENTRY3,ENTRY4"
-kd -c mykube -n testing -f split.yaml --debug-templates --dryrun
+#kd -c mykube -n testing -f split.yaml --debug-templates --dryrun
 
-export BAR="${PWD}/config.yaml"
-kd -c mykube -n testing -f file.yaml --debug-templates --dryrun
+export BAR="${PWD}/vars/config"
+#kd -c mykube -n testing -f file.yaml --debug-templates --dryrun
 
 echo "Dry run example complete"
 
+kd -f . --debug-templates --dryrun
